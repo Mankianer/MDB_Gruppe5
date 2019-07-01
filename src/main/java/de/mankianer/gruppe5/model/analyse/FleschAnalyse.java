a0c252ee49f5a1760206e26395f52683c7161ba9
meichholz <tim.eichholz98@web.de>
2019-07-01 11:44:46 +0200

    flesch index

---
+++ src/main/java/de/mankianer/gruppe5/model/analyse/FleschAnalyse.java
@@ -0,0 +1,8 @@
+package de.mankianer.gruppe5.model.analyse;
+
+public class FleschAnalyse extends Analyse{
+    // 0 (leicht) - 100 (schwer)
+    public FleschAnalyse(double score) {
+        super("" + score);
+    }
+}
